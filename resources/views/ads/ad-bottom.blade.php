fc77145d6e471ebd046bf92d2a06d21474eb1740
Tomasz Warężak <t.warezak@gmail.com>
2023-11-15 17:24:50 +0100

    add ads

---
+++ resources/views/ads/ad-bottom.blade.php
@@ -0,0 +1,19 @@
+<div class="my-5 text-center">
+    @if ($appEnv === 'prod')
+        @if ($adsEnabled)
+            @if ($userAgent->isMobile())
+
+            @else
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:300px;height:600px"
+                     data-ad-client="ca-pub-9047305524185109"
+                     data-ad-slot="9699556503"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            @endif
+        @endif
+    @else
+        reklama bottom
+    @endif
+</div>
